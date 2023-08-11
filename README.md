04cb40899a9618f30318906fc58a1cf8bae6ba62
박준현 <4rne5@duck.com>
2023-08-11 22:18:59 +0900

    V 1.0 Released
    GG

---
+++ README.md
@@ -0,0 +1,24 @@
+# Twitch Timeout Count & Rank Bot
+## a.k.a. 악질봇
+***
+## Command & Function
+***
+> Stock Function : 시청자가 강퇴되면 "{사용자명}게이야...넌 나가라"를 말합니다!
+
+> **!timeoutrank!**
+> : 시청자의 타임아웃 랭킹을 보여줍니다
+
+## Step To Use
+***
+1. ```pip install -r requirements.txt``` to Install Requirement Libraries
+2. Make ```config.json``` and Make Like That
+```json
+{
+    "token": "{TWITCH_OAUTH_TOKEN}",
+    "cid": "{TWITCH_CLIENT_ID}"
+}
+```
+> **How to Get This Token and Client ID?** : https://tgd.kr/c/programming/27582052
+3. Edit 29 and 65 Line's **Streamer Name** **(!!My Target Line is Based On Stock Code!!)**
+4. Run ```main.py``` and Use **악질봇**.
+
--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import asyncio
+# Default Code Made By 4rNe5
+# This Code is MIT Licence.
+# My Github : @4rNe5
+# About 4rne5 : 4rne5.kro.kr
+
 import json
 from twitchio.ext import commands
 from collections import defaultdict
@@ -7,49 +11,62 @@ from collections import defaultdict
 timeouts = defaultdict(int)
 
 # import Twitch Bot Setting  -> define Class
-
 with open("./config.json", "r", encoding="utf-8") as E:
   config = json.loads(E.read())
 
-irc = config['irc']
+token = config['token']
 cid = config['cid']
+
 
 class TwitchBot(commands.Bot):
 
-    def __init__(self):
-        super().__init__(
-            token=irc, # Twitch OAuth Token - Secret
-            client_id=cid, # Twitch Client ID - Secret
-            nick='악질봇', # Bot's Twitch Nick
-            prefix='!', #Bot Command Start emoji
-            initial_channels=['마야100'] # Streamer's Name for Bot's Tracking
-        )
+  def __init__(self):
+    super().__init__(
+      token = token,  # Twitch OAuth Token - Secret
+      client_id = cid,  # Twitch Client ID - Secret
+      nick = '악질봇',  # Bot's Twitch Nick
+      prefix = '!',  # Bot Command Start emoji
+      initial_channels = ['마야100']  # Streamer's Name for Bot's Tracking
+    )
 
-    # MEssage Event Method
-    async def event_message(self, message):
-        # Run Func when receive message
-        await self.handle_commands(message)
+  # MEssage Event Method -> when msg comes
+  async def event_message(self, message):
+    # Run Func when receive message
+    await self.handle_commands(message)
 
-    # define timeout ranking command -> !timeoutrank
-    @commands.command(name='timeoutrank')
-    async def timeout_stats(self, ctx):
-        timeout_ranking = sorted(timeouts.items(), key=lambda x: x[1], reverse=True)[:10]
+  async def check_permissions(self, ctx):
+    broadcaster = ctx.channel.is_broadcaster(ctx.author)  # Check Streamer Permission
+    moderator = ctx.channel.is_moderator(ctx.author)  # Check Manager Permission
 
-        response = "타임아웃 랭킹 : "
-        for idx, (user, count) in enumerate(timeout_ranking):
-            response += f"\n{idx+1}. {user}: {count}회"
+    if broadcaster or moderator:
+      return True
+    else:
+      await ctx.send("명령어 사용 권한이 없습니다.")
+      return False
 
-        await ctx.send(response)
+  # define timeout ranking command -> !timeoutrank
+  @commands.command(name='timeoutrank')
+  async def timeout_stats(self, ctx):
+    if not await self.check_permissions(ctx):  # Checking Permission
+      return
 
-    # define timeout tracking event method
-    async def event_clearchat(self, params):
-      user = params['target_user']
-      timeouts[user] += 1
-      channel = self.get_channel('마야100')  # 봇이 타임아웃을 추적할 스트리머 채널 이름으로 교체하세요.
-      await channel.send(f"{user}님이 {timeouts[user]}번째로 나갔습니다...")
+    timeout_ranking = sorted(timeouts.items(), key=lambda x: x[1], reverse=True)[:10]
+
+    response = "타임아웃 랭킹 : "
+    for idx, (user, count) in enumerate(timeout_ranking):
+      response += f"\n{idx + 1}. {user}: {count}회"
+
+    await ctx.send(response)
+
+  # define timeout tracking event method
+  async def event_clearchat(self, params):
+    user = params['target_user']
+    timeouts[user] += 1
+    channel = self.get_channel('마야100')  # Replace with the name of the streamer's channel you want to track timeouts
+    await channel.send(f"{user}게이야...넌 나가라")
 
 
 # Bot Run
 if __name__ == "__main__":
-    bot = TwitchBot()
-    bot.run()
+  bot = TwitchBot()
+  bot.run()
